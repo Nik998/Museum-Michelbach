f6a7a29a4ca3f7c25169a964a4b28fd4c35bf832
Niklas Seither <s222792@student.dhbw-mannheim.de>
2023-12-24 00:29:15 +0100

    Removed unnecessary file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,44 @@
 <template>
-  <Home/>
+  <AppBar/>
+
+  <v-container class="main-container"
+               :fluid="true">
+    <v-card class="mb-4 pt-2 pb-4 bg-secondary">
+      <v-card-title class="font-weight-medium bg-secondary">Hörbeispiele</v-card-title>
+      <v-card-subtitle>Bitte wählen Sie einen Raum</v-card-subtitle>
+    </v-card>
+
+    <v-row>
+      <v-col v-for="item in rooms"
+             :key="item.id"
+             cols="12"
+             sm="12"
+             md="6"
+             lg="6"
+             xl="4"
+             xxl="3">
+        <CardRooms :item="item"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script setup>
-import Home from '@/components/Home.vue'
+<script>
+import AppBar from "@/components/utils/AppBar.vue"
+import CardRooms from "@/components/utils/CardRooms.vue"
+import {Room} from "@/main.js";
+
+export default {
+  components: {
+    AppBar,
+    CardRooms,
+  },
+  data() {
+    return {
+      rooms: [
+        new Room("Raum 1", "Raum 1", "Das ist eine Kurzbeschreibung", "Das ist eine ausführliche Beschreibung"),
+      ]
+    }
+  }
+}
 </script>
